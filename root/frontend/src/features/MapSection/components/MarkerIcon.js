0d23a2d0914794720e68ccd838bb648c893ecd53
mohamedyasser27 <my2722000@gmail.com>
2023-02-20 22:14:09 +0200

    Fix marker icon
    it was moving around on zoom because i didn't specify an
    anchor which depends on the size so i specified both

--- root/frontend/src/features/MapSection/components/MarkerIcon.js
+++ root/frontend/src/features/MapSection/components/MarkerIcon.js
@@ -4,5 +4,8 @@ import shadowpng from "../assets/marker-shadow.png";
 const markerIcon = L.icon({
   iconUrl: markerpng,
   shadowUrl: shadowpng,
+  iconSize: [24, 36],
+  iconAnchor: [12, 36],
+  shadowAnchor: [12, 36],
 });
 export default markerIcon;
